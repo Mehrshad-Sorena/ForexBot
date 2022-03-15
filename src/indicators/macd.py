44a590d500fee255aac8ff33cf397e778df6b0c6
Mehrshad_Sorena <m.kahrizy1990@gmail.com>
2022-03-15 11:49:52 +0330

    Update macd.py

--- src/indicators/macd.py
+++ src/indicators/macd.py
@@ -25,8 +25,11 @@ import matplotlib.pyplot as plt
 #help(ta.macd)
 
 #**************************************************** Golden Cross Strategy *******************************************************
-def Golden_Cross_SMA(dataset,Apply_to,symbol,macd_fast=12,macd_slow=26,macd_signal=9,plot=False):
+def Golden_Cross_SMA(dataset,Apply_to,symbol,macd_fast=12,macd_slow=26,macd_signal=9,mode='optimize',plot=False):
 
+	#Mode:
+	#optimize
+	#online
 	macd_read = ind.macd(dataset[symbol][Apply_to],fast = macd_fast,slow = macd_slow,signal = macd_signal)
 
 	macd = pd.DataFrame()
@@ -64,75 +67,76 @@ def Golden_Cross_SMA(dataset,Apply_to,symbol,macd_fast=12,macd_slow=26,macd_sign
 		if (plot == True):
 			plt.plot(cross['index'],cross['values'], 'o',c='g')
 
-	signal_buy = pd.DataFrame(np.zeros(len(cross)))
-	signal_buy['signal'] = np.nan
-	signal_buy['values'] = np.nan
-	signal_buy['index'] = np.nan
-	signal_buy['profit'] = np.nan
+	if(mode == 'online'):
+		signal_buy = pd.DataFrame(np.zeros(len(cross)))
+		signal_buy['signal'] = np.nan
+		signal_buy['values'] = np.nan
+		signal_buy['index'] = np.nan
 
-	signal_sell = pd.DataFrame(np.zeros(len(cross)))
-	signal_sell['signal'] = np.nan
-	signal_sell['values'] = np.nan
-	signal_sell['index'] = np.nan
-	signal_sell['profit'] = np.nan
+		signal_sell = pd.DataFrame(np.zeros(len(cross)))
+		signal_sell['signal'] = np.nan
+		signal_sell['values'] = np.nan
+		signal_sell['index'] = np.nan
 
+		buy_indexes = cross['index'][np.where(((macd.macds[cross['index']-1].to_numpy()>macd.macd[cross['index']-1].to_numpy())&(macd.macds[cross['index']+1].to_numpy()<macd.macd[cross['index']+1].to_numpy())))[0]]
+		buy_cross_indexes = np.where(((macd.macds[cross['index']-1].to_numpy()>macd.macd[cross['index']-1].to_numpy())&(macd.macds[cross['index']+1].to_numpy()<macd.macd[cross['index']+1].to_numpy())))[0]
+		signal_buy['signal'][buy_cross_indexes] = 'buy'
+		signal_buy['values'] = cross['values'][buy_cross_indexes]
+		signal_buy['index'][buy_cross_indexes] = buy_indexes
+
+		sell_indexes = cross['index'][np.where(((macd.macds[cross['index']-1].to_numpy()<macd.macd[cross['index']-1].to_numpy())&(macd.macds[cross['index']+1].to_numpy()>macd.macd[cross['index']+1].to_numpy())))[0]]
+		sell_cross_indexes = np.where(((macd.macds[cross['index']-1].to_numpy()<macd.macd[cross['index']-1].to_numpy())&(macd.macds[cross['index']+1].to_numpy()>macd.macd[cross['index']+1].to_numpy())))[0]
+		signal_sell['signal'][sell_cross_indexes] = 'sell'
+		signal_sell['values'] = cross['values'][sell_cross_indexes]
+		signal_sell['index'][sell_cross_indexes] = sell_indexes
+
+	#print(buy_indexes)
+	#print(signal_buy)
+
+	#
 	i = 0
 	j = 0
 	k = 0
 
-	print(cross['index'][32])
+	if(mode == 'optimize'):
+		signal_buy = pd.DataFrame(np.zeros(len(cross)))
+		signal_buy['signal'] = np.nan
+		signal_buy['values'] = np.nan
+		signal_buy['index'] = np.nan
+		signal_buy['profit'] = np.nan
 
-	plt.plot(macd.index,macd.macd,c = 'b')
-	plt.plot(macd.index,macd.macds,c = 'r')
+		signal_sell = pd.DataFrame(np.zeros(len(cross)))
+		signal_sell['signal'] = np.nan
+		signal_sell['values'] = np.nan
+		signal_sell['index'] = np.nan
+		signal_sell['profit'] = np.nan
 
-	buy_indexes = cross['index'][np.where(((macd.macds[cross['index']-1].to_numpy()>macd.macd[cross['index']-1].to_numpy())&(macd.macds[cross['index']+1].to_numpy()<macd.macd[cross['index']+1].to_numpy())))[0]]
-	buy_cross_indexes = np.where(((macd.macds[cross['index']-1].to_numpy()>macd.macd[cross['index']-1].to_numpy())&(macd.macds[cross['index']+1].to_numpy()<macd.macd[cross['index']+1].to_numpy())))[0]
-	signal_buy['signal'][buy_cross_indexes] = 'buy'
-	signal_buy['values'] = cross['values'][buy_cross_indexes]
-	signal_buy['index'][buy_cross_indexes] = buy_indexes
+		for elm in cross['index']:
+			if ((macd.macds[elm-1]>macd.macd[elm-1])&(macd.macds[elm+1]<macd.macd[elm+1])):
+				signal_buy['signal'][i] = 'buy'
+				signal_buy['values'][i] = cross['values'][j]
+				signal_buy['index'][i] = elm
 
-	print(buy_indexes)
+				if ((j+1) < len(cross)):
+					signal_buy['profit'][i] = (np.max(dataset[symbol]['close'][elm:cross['index'][j+1]] - dataset[symbol]['close'][elm])/dataset[symbol]['close'][elm]) * 100
+				else:
+					signal_buy['profit'][i] = (np.max(dataset[symbol]['close'][elm:-1] - dataset[symbol]['close'][elm])/dataset[symbol]['close'][elm]) * 100
+				i += 1
 
-	#if ((buy_cross_indexes+1) < len(cross)):
-	#	signal_buy['profit'] = (np.max(dataset[symbol]['close'][buy_indexes:cross['index'][buy_cross_indexes+1]] - dataset[symbol]['close'][buy_indexes])/dataset[symbol]['close'][buy_indexes]) * 100
-	#else:
-	close = dataset[symbol]['close']
-	print('index buy = ',buy_indexes.values)
-	print('index cross last = ',cross['index'][buy_cross_indexes])
-	print(((close[buy_indexes.values[0]:cross['index'][buy_cross_indexes[0]+1]] - close[buy_indexes.values[0]])/dataset[symbol]['close'][buy_indexes.values[0]]))
-	signal_buy['profit'] = (np.max((close[buy_indexes.values[0]:-1] - close[buy_indexes.values[0]])/dataset[symbol]['close'][buy_indexes.values[0]])) * 100
+			if ((macd.macds[elm-1]<macd.macd[elm-1])&(macd.macds[elm+1]>macd.macd[elm+1])):
+				signal_sell['signal'][k] = 'sell'
+				signal_sell['values'][k] = cross['values'][j]
+				signal_sell['index'][k] = elm
+				if ((j+1) < len(cross)):
+					signal_sell['profit'][k] = (np.max(dataset[symbol]['close'][elm] - dataset[symbol]['close'][elm:cross['index'][j+1]])/np.min(dataset[symbol]['close'][elm:cross['index'][j+1]])) * 100
+				else:
+					signal_sell['profit'][k] = (np.max(dataset[symbol]['close'][elm] - dataset[symbol]['close'][elm:-1])/np.min(dataset[symbol]['close'][elm:-1])) * 100
+				#print('elm_sell = ',elm)
+				k += 1
+			j += 1
 
-	#signal_buy['profit'][buy_cross_indexes] = (np.max(dataset[symbol]['close'][buy_indexes:cross['index'][buy_cross_indexes+1]] - dataset[symbol]['close'][buy_indexes])/dataset[symbol]['close'][buy_indexes]) * 100
-	print(signal_buy)
-
-	for a in buy_indexes:
-		plt.axvline(x=a, color='g', linestyle='-')
-
-	#plt.show()
-	for elm in cross['index']:
-		if ((macd.macds[elm-1]>macd.macd[elm-1])&(macd.macds[elm+1]<macd.macd[elm+1])):
-			signal_buy['signal'][i] = 'buy'
-			signal_buy['values'][i] = cross['values'][j]
-			signal_buy['index'][i] = elm
-
-			if ((j+1) < len(cross)):
-				signal_buy['profit'][i] = (np.max(dataset[symbol]['close'][elm:cross['index'][j+1]] - dataset[symbol]['close'][elm])/dataset[symbol]['close'][elm]) * 100
-			else:
-				signal_buy['profit'][i] = (np.max(dataset[symbol]['close'][elm:-1] - dataset[symbol]['close'][elm])/dataset[symbol]['close'][elm]) * 100
-			i += 1
-
-		if ((macd.macds[elm-1]<macd.macd[elm-1])&(macd.macds[elm+1]>macd.macd[elm+1])):
-			signal_sell['signal'][k] = 'sell'
-			signal_sell['values'][k] = cross['values'][j]
-			signal_sell['index'][k] = elm
-			if ((j+1) < len(cross)):
-				signal_sell['profit'][k] = (np.max(dataset[symbol]['close'][elm] - dataset[symbol]['close'][elm:cross['index'][j+1]])/np.min(dataset[symbol]['close'][elm:cross['index'][j+1]])) * 100
-			else:
-				signal_sell['profit'][k] = (np.max(dataset[symbol]['close'][elm] - dataset[symbol]['close'][elm:-1])/np.min(dataset[symbol]['close'][elm:-1])) * 100
-			#print('elm_sell = ',elm)
-			k += 1
-		j += 1
-
+	if (plot == True):
+		plt.show()
 
 	signal_buy = signal_buy.dropna(inplace = False)
 	signal_sell = signal_sell.dropna(inplace = False)
@@ -151,8 +155,10 @@ def Golden_Cross_SMA(dataset,Apply_to,symbol,macd_fast=12,macd_slow=26,macd_sign
 
 #*********************************** How To Use Funcs ************************************************************
 
-symbol_data_5M,money,sym = log_get_data_Genetic(mt5.TIMEFRAME_M5,0,1000)
+symbol_data_5M,money,sym = log_get_data_Genetic(mt5.TIMEFRAME_M5,0,20000)
 
-signal = Golden_Cross_SMA(dataset=symbol_data_5M,Apply_to='close',symbol='AUDCAD_i',macd_fast=12,macd_slow=26,macd_signal=9,plot=False)
-#print(signal)
+signal_buy,signal_sell = Golden_Cross_SMA(dataset=symbol_data_5M,Apply_to='close',symbol='AUDCAD_i',
+	macd_fast=12,macd_slow=26,macd_signal=9,mode='optimize',plot=False)
+print(signal_buy)
+print(signal_sell)
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////

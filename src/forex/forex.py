c26c5c5aa746d03fa2fca7878690947d0e8f362c
rasoul-aibees <rasoul@ai-bees.io>
2022-03-17 11:49:10 +0330

    merge russell in master

--- src/forex/forex.py
+++ src/forex/forex.py
@@ -1,11 +1,38 @@
-#import MetaTrader5 as mt5
+from src.utils import GetData, bestExtremeFinder
+from src.strategy import sikimi
 from src.db import ForexMongo
+import MetaTrader5 as mt5
 from .login import Login
 
 
-class ForexRobot(ForexMongo, Login):
+class ForexRobot(Login, GetData):
+    """
+    Forex Robot
+    """
     def __init__(self, *args, **kwargs):
-        account = kwargs.get('account')
+        self.strategy = kwargs.get('strategy')
+        self.indicator = kwargs.get('indicator')
 
-        strategy = kwargs.get('strategy')
-        indicator = kwargs.get('indicator')
+    def run(self):
+        if self.strategy == 'sikimi':
+            symbol_data_5M, money, sym = self.getGenetic(
+                    mt5.TIMEFRAME_M5, 0, 200)
+            symbol_data_15M, money, sym = self.getGenetic(
+                    mt5.TIMEFRAME_M15, 0, 6000)
+            symbol_data_1H, money, sym = self.getGenetic(
+                    mt5.TIMEFRAME_H1, 0, 2000)
+            symbol_data_4H, money, sym = self.getGenetic(
+                    mt5.TIMEFRAME_H4, 0, 360)
+            symbol_data_1D, money, sym = self.getGenetic(
+                    mt5.TIMEFRAME_D1, 0, 60)
+            exterm_point_pred = self.extremePointsIchimoko(
+                    mt5.TIMEFRAME_M5, 0, 200)
+
+            y3 = symbol_data_5M['AUDCAD_i']['high']
+            y4 = symbol_data_5M['AUDCAD_i']['low']
+
+            sikimi_strategy_result = sikimi(
+                    y3, y4, symbol_data_5M, symbol_data_15M, symbol_data_1D,
+                    symbol_data_1H, symbol_data_4H, exterm_point_pred)
+
+            return sikimi_strategy_result
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,6 @@
 from .logger import logs
-from .resist_protect import Extreme_points, Extreme_points_ichimoko, extreme_points_ramp_lines, Best_Extreme_Finder, protect_resist
+from .resist_protect import (
+        extremePointsIchimoko,
+        bestExtremeFinder,
+        extremePoints,
+)

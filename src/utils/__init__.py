4712788b51827b40df97249bf3d45be523ec4d6c
rasoul-aibees <rasoul@ai-bees.io>
2022-03-23 13:45:22 +0430

    update resist_protect in utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,8 @@
 from .logger import logs
 from .resist_protect import (
-        extremePointsIchimoko,
-        bestExtremeFinder,
-        extremePoints,
+        extreme_points,
+        extreme_points_ichimoko,
+        extreme_points_ramp_lines,
+        best_extreme_finder,
+        protect_resist,
 )

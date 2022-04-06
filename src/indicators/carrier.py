b3d4c4b5ebfd0b0d4a85e5f8fba93d19cb004aba
Mehrshad_Sorena <m.kahrizy1990@gmail.com>
2022-04-06 14:53:56 +0430

    Create carrier.py

---
+++ src/indicators/carrier.py
@@ -0,0 +1,116 @@
+from log_get_data import *
+import MetaTrader5 as mt5
+
+def carrier_buy(symbol,lot,st,tp,comment,magic):
+
+	point = mt5.symbol_info(symbol).point
+	price_ask = mt5.symbol_info_tick(symbol).ask
+	price = mt5.symbol_info_tick(symbol).ask
+	price_bid = mt5.symbol_info_tick(symbol).bid
+	spred = ((abs(price_ask-price_bid)/price_ask) * 100)
+	deviation = 5
+
+	if (spred > 0.045): return
+
+	if (tp <= (price)): return
+
+	if (st >= price_bid): return
+
+	print(symbol,': ','buy')
+	request = {
+		"action": mt5.TRADE_ACTION_DEAL,
+    	"symbol": symbol,
+    	"volume": lot,
+    	"type": mt5.ORDER_TYPE_BUY,
+    	"price": price,
+    	"sl": st,
+    	"tp": tp,
+    	"deviation": deviation,
+    	"magic": magic,
+    	"comment": comment,
+    	"type_time": mt5.ORDER_TIME_GTC,
+    	"type_filling": mt5.ORDER_FILLING_FOK
+   		 }
+
+	result = mt5.order_send(request)
+	# check the execution result
+	print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol,lot,price,deviation));
+
+	try:
+		if result.retcode != mt5.TRADE_RETCODE_DONE:
+			print("2. order_send failed, retcode={}".format(result.retcode))
+			# request the result as a dictionary and display it element by element
+			result_dict=result._asdict()
+			for field in result_dict.keys():
+				print("   {}={}".format(field,result_dict[field]))
+        		# if this is a trading request structure, display it element by element as well
+				if field=="request":
+					traderequest_dict=result_dict[field]._asdict()
+					for tradereq_filed in traderequest_dict:
+						print("       traderequest: {}={}".format(tradereq_filed,traderequest_dict[tradereq_filed]))
+				else:
+					print('send Done ',comment)
+	except:
+		print('some thing wrong send ',comment)
+
+	return
+
+def carrier_sell(symbol,lot,st,tp,comment,magic):
+
+	price_ask = mt5.symbol_info_tick(symbol).ask
+	price = mt5.symbol_info_tick(symbol).bid
+	price_bid = mt5.symbol_info_tick(symbol).bid
+	spred = ((abs(price_ask-price_bid)/price_ask) * 100)
+	deviation = 5
+
+	if (spred > 0.045):
+		print('re')
+		return
+
+	if (st <= price_ask):
+		print('tu')
+		return
+
+	if (tp >= (price_bid)):
+		print('rn')
+		return
+
+	print(symbol,': ','sell')
+	request = {
+		"action": mt5.TRADE_ACTION_DEAL,
+    	"symbol": symbol,
+    	"volume": lot,
+    	"type": mt5.ORDER_TYPE_SELL,
+    	"price": price,
+    	"sl": st,
+    	"tp": tp,
+    	"deviation": deviation,
+    	"magic": magic,
+    	"comment": comment,
+    	"type_time": mt5.ORDER_TIME_GTC,
+    	"type_filling": mt5.ORDER_FILLING_FOK
+    	}
+
+	result = mt5.order_send(request)
+	# check the execution result
+	print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol,lot,price,deviation));
+
+	try:
+		if result.retcode != mt5.TRADE_RETCODE_DONE:
+			print("2. order_send failed, retcode={}".format(result.retcode))
+			# request the result as a dictionary and display it element by element
+			result_dict=result._asdict()
+			for field in result_dict.keys():
+				print("   {}={}".format(field,result_dict[field]))
+        	# if this is a trading request structure, display it element by element as well
+				if field=="request":
+					traderequest_dict=result_dict[field]._asdict()
+					for tradereq_filed in traderequest_dict:
+						print("       traderequest: {}={}".format(tradereq_filed,traderequest_dict[tradereq_filed]))
+				else:
+					print('send Done ',comment)
+	except:
+		print('some thing wrong send ',comment)
+
+	return
+

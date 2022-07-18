6b0cca30c6753ec1fcf3f99f272944142b322db9
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-19 03:56:32 +0430

    Update pr_test.py

--- src/indicators/pr_test.py
+++ src/indicators/pr_test.py
@@ -1,7 +1,11 @@
 from multiprocessing.spawn import import_main_path
 from pr_ExtremePoints import ExtremePoints
+from pr_TrendLines import TrendLines
 from pr_Parameters import Parameters
 from pr_Config import Config
+import numpy as np
+from timer import stTime
+#from pr_Runner import Runner
 from log_get_data import *
 import MetaTrader5 as mt5
 import sys
@@ -12,24 +16,47 @@ data_1H,money,_  = log_get_data_Genetic(mt5.TIMEFRAME_H1,0,500)
 parameters = Parameters()
 parameters.elements['dataset_5M'] = pd.DataFrame(data_5M['XAUUSD_i'])
 parameters.elements['dataset_1H'] = pd.DataFrame(data_1H['XAUUSD_i'])
-parameters.elements['ExtremePoints_num_max_5M'] = 50
-parameters.elements['ExtremePoints_num_min_5M'] = 50
-parameters.elements['ExtremePoints_num_max_1H'] = 10
-parameters.elements['ExtremePoints_num_min_1H'] = 10
+parameters.elements['ExtremePoints_num_max_5M'] = 5
+parameters.elements['ExtremePoints_num_min_5M'] = 5
+parameters.elements['ExtremePoints_num_max_1H'] = 100
+parameters.elements['ExtremePoints_num_min_1H'] = 100
 
 parameters.elements['ExtremePoints_weight_5M'] = 200
 parameters.elements['ExtremePoints_weight_1H'] = 200
 
 config = Config()
 config.cfg['ExtremePoints_T_5M'] = True
-config.cfg['ExtremePoints_T_1H'] = False
+config.cfg['ExtremePoints_T_1H'] = True
 config.cfg['ExtremePoints_status'] = True
+
+#pr_Runner = Runner(parameters = parameters, config = config)
+#pr_Runner.start()
 
 extreme_points = ExtremePoints(parameters = parameters, config = config)
 
-
 extreme_points.get(timeframe = '5M')
-extreme_points.ploter()
+#extreme_points.ploter()
+
+
+parameters.elements['TrendLines_num_max_5M'] = 5
+parameters.elements['TrendLines_num_min_5M'] = 5
+parameters.elements['TrendLines_num_max_1H'] = 5
+parameters.elements['TrendLines_num_min_1H'] = 5
+
+parameters.elements['TrendLines_weight_5M'] = 200
+parameters.elements['TrendLines_weight_1H'] = 200
+
+config.cfg['TrendLines_T_5M'] = True
+config.cfg['TrendLines_T_1H'] = True
+config.cfg['TrendLines_status'] = True
+
+find = TrendLines(parameters = parameters, config = config)
+
+find.get(
+		length='long',
+		timeframe='5M'
+		)
+#find.ploter(length='mid')
 
 
 

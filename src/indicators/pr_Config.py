a3a0d6c8ac491cde032669a6af002f5a797cbc5b
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-18 19:20:00 +0430

    Create pr_Config.py

---
+++ src/indicators/pr_Config.py
@@ -0,0 +1,12 @@
+class Config:
+
+	def __init__(cls):
+		
+		cls.cfg = dict(
+						{
+						'ExtremePoints_status': True,
+						'ExtremePoints_T_5M': False,
+						'ExtremePoints_T_1H': False,
+						'plot': False,
+						}
+						)

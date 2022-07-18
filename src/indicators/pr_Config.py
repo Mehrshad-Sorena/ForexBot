f3e7f4513f1c0ab8ee9aa1d2a434c4ffa136280a
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-19 03:56:11 +0430

    Update pr_Config.py

--- src/indicators/pr_Config.py
+++ src/indicators/pr_Config.py
@@ -4,9 +4,22 @@ class Config:
 		
 		cls.cfg = dict(
 						{
+						#Config For ExtremePoints:
 						'ExtremePoints_status': True,
 						'ExtremePoints_T_5M': False,
 						'ExtremePoints_T_1H': False,
+						#///////////////////////////
+
+
+						#Config For TrendLines:
+						'TrendLines_status': True,
+						'TrendLines_T_5M': True,
+						'TrendLines_T_1H': True,
+						'TrendLines_plot': False,
+						#///////////////////////////
+
+
+						#Config For
 						'plot': False,
 						}
 						)

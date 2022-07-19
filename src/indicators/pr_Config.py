46e867fee124553ad5faeaff0764ef5d00e1837b
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-19 20:38:04 +0430

    Update pr_Config.py

--- src/indicators/pr_Config.py
+++ src/indicators/pr_Config.py
@@ -13,13 +13,36 @@ class Config:
 
 						#Config For TrendLines:
 						'TrendLines_status': True,
+
 						'TrendLines_T_5M': True,
+
+						'TrendLines_long_T_5M': True,
+						'TrendLines_mid_T_5M': True,
+						'TrendLines_short1_T_5M': True,
+						'TrendLines_short2_T_5M': True,
+
 						'TrendLines_T_1H': True,
+
+						'TrendLines_long_T_1H': True,
+						'TrendLines_mid_T_1H': True,
+						'TrendLines_short1_T_1H': True,
+						'TrendLines_short2_T_1H': True,
+
 						'TrendLines_plot': False,
 						#///////////////////////////
 
+						#Config For IchimokouFlatLines:
+
+						'IchimokouFlatLines' + '_T_5M': True,
+						'IchimokouFlatLines' + '_T_1H': True,
+						'IchimokouFlatLines' + '_status': True,
+
+						'IchimokouFlatLines_plot': True,
+
+						#//////////////////////////
+
 
 						#Config For
-						'plot': False,
+						'plot': True,
 						}
 						)

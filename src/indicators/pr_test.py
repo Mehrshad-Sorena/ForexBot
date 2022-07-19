74e1f831a2729ed043e1e10b0c96fffc7356f7d4
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-19 20:38:27 +0430

    Update pr_test.py

--- src/indicators/pr_test.py
+++ src/indicators/pr_test.py
@@ -1,21 +1,20 @@
 from multiprocessing.spawn import import_main_path
 from pr_ExtremePoints import ExtremePoints
+from pr_IchimokouFlatLines import IchimokouFlatLines
 from pr_TrendLines import TrendLines
 from pr_Parameters import Parameters
 from pr_Config import Config
 import numpy as np
 from timer import stTime
-#from pr_Runner import Runner
+from pr_Runner import Runner
 from log_get_data import *
 import MetaTrader5 as mt5
 import sys
 
-data_5M,money,_  = log_get_data_Genetic(mt5.TIMEFRAME_M5,0,600)
-data_1H,money,_  = log_get_data_Genetic(mt5.TIMEFRAME_H1,0,500)
+data_5M,money,_  = log_get_data_Genetic(mt5.TIMEFRAME_M5,0,800)
+data_1H,money,_  = log_get_data_Genetic(mt5.TIMEFRAME_H1,0,600)
 
 parameters = Parameters()
-parameters.elements['dataset_5M'] = pd.DataFrame(data_5M['XAUUSD_i'])
-parameters.elements['dataset_1H'] = pd.DataFrame(data_1H['XAUUSD_i'])
 parameters.elements['ExtremePoints_num_max_5M'] = 5
 parameters.elements['ExtremePoints_num_min_5M'] = 5
 parameters.elements['ExtremePoints_num_max_1H'] = 100
@@ -31,7 +30,7 @@ config.cfg['ExtremePoints_status'] = True
 
 #pr_Runner = Runner(parameters = parameters, config = config)
 #pr_Runner.start()
-
+"""
 extreme_points = ExtremePoints(parameters = parameters, config = config)
 
 extreme_points.get(timeframe = '5M')
@@ -50,13 +49,19 @@ config.cfg['TrendLines_T_5M'] = True
 config.cfg['TrendLines_T_1H'] = True
 config.cfg['TrendLines_status'] = True
 
-find = TrendLines(parameters = parameters, config = config)
+trendline = TrendLines(parameters = parameters, config = config)
 
-find.get(
-		length='long',
-		timeframe='5M'
-		)
-#find.ploter(length='mid')
+trendline.runner(timeframe='1H')
+#trendline.ploter(length='mid')
+
+flatlines = IchimokouFlatLines(parameters = parameters, config = config)
+
+#flatlines.ploter()
+"""
+
+pr_Runner = Runner(parameters = parameters, config = config)
+print(pr_Runner.start(dataset_5M = pd.DataFrame(data_5M['XAUUSD_i']), dataset_1H = pd.DataFrame(data_1H['XAUUSD_i']),loc_end_5M=700))
+#pr_Runner.ploter(dataset_5M = pd.DataFrame(data_5M['XAUUSD_i']), dataset_1H = pd.DataFrame(data_1H['XAUUSD_i']),loc_end_5M=700)
 
 
 

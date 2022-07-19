5dc0b52b8bacbd7f3b6160e0e735d4fd2906a885
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-19 20:47:13 +0430

    Update pr_BestFinder.py

--- src/indicators/pr_BestFinder.py
+++ src/indicators/pr_BestFinder.py
@@ -454,29 +454,3 @@ class BestFinder:
 				Power_Mid_Line_low = exterm_point_pred_final_low['power'][kmeans_low.predict(Mid_Line_low.reshape(1,-1))].to_numpy()/np.max(exterm_point_pred_final_low['power'])
 			
 			"""
-
-# from log_get_data import *
-# import MetaTrader5 as mt5
-# import sys
-
-# data_5M,money,_  = log_get_data_Genetic(mt5.TIMEFRAME_M5,0,600)
-# data_1H,money,_  = log_get_data_Genetic(mt5.TIMEFRAME_H1,0,500)
-
-# parameters = Parameters()
-# parameters.elements['dataset_5M'] = pd.DataFrame(data_5M['XAUUSD_i'])
-# parameters.elements['dataset_1H'] = pd.DataFrame(data_1H['XAUUSD_i'])
-# parameters.elements['ExtremePoints_num_max_5M'] = 5
-# parameters.elements['ExtremePoints_num_min_5M'] = 5
-# parameters.elements['ExtremePoints_num_max_1H'] = 100
-# parameters.elements['ExtremePoints_num_min_1H'] = 100
-
-# parameters.elements['ExtremePoints_weight_5M'] = 200
-# parameters.elements['ExtremePoints_weight_1H'] = 200
-
-# config = Config()
-# config.cfg['ExtremePoints_T_5M'] = True
-# config.cfg['ExtremePoints_T_1H'] = True
-# config.cfg['ExtremePoints_status'] = True
-
-# bestfinder = BestFinder(parameters = parameters, config = config)
-# bestfinder.finder(extermpoint = pd.DataFrame(),timeframe = '5M')

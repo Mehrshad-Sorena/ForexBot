9ca93b10960b1e0d22a9710507288950a41322c5
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-20 21:21:26 +0430

    Update DataChanger.py

--- src/indicators/DataChanger.py
+++ src/indicators/DataChanger.py
@@ -18,45 +18,7 @@ class DataChanger:
 
 			cut_first = int(len(data.low)-1) - length
 
-		data_splited['low'] = data.low[
-										cut_first:int(len(data.low)-1)
-										].reset_index(drop=True)
-
-		data_splited['high'] = data.high[
-										cut_first:int(len(data.high)-1)
-										].reset_index(drop=True)
-
-		data_splited['close'] = data.close[
-											cut_first:int(len(data.close)-1)
-											].reset_index(drop=True)
-
-		data_splited['open'] = data.open[
-										cut_first:int(len(data.open)-1)
-										].reset_index(drop=True)
-
-		data_splited['time'] = data['time'][
-										cut_first:int(len(data['time'])-1)
-										].reset_index(drop=True)
-
-		data_splited['volume'] = data['volume'][
-										cut_first:int(len(data['volume'])-1)
-										].reset_index(drop=True)
-
-		data_splited['OHLC/4'] = data['OHLC/4'][
-										cut_first:int(len(data['OHLC/4'])-1)
-										].reset_index(drop=True)
-
-		data_splited['HLCC/4'] = data['HLCC/4'][
-										cut_first:int(len(data['HLCC/4'])-1)
-										].reset_index(drop=True)
-
-		data_splited['HLC/3'] = data['HLC/3'][
-										cut_first:int(len(data['HLC/3'])-1)
-										].reset_index(drop=True)
-
-		data_splited['HL/2'] = data['HL/2'][
-										cut_first:int(len(data['HL/2'])-1)
-										].reset_index(drop=True)
+		data_splited = data.truncate(before=cut_first, after=int(len(data.low)-1), axis=None, copy=True).reset_index(drop=True)
 
 		return data_splited
 
@@ -75,23 +37,14 @@ class DataChanger:
 		if (loc_end_5M > length_5M):
 			cut_first = int(loc_end_5M) - length_5M
 
-		dataset_pr_5M['low'] = dataset_5M['low'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_5M['high'] = dataset_5M['high'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_5M['close'] = dataset_5M['close'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_5M['open'] = dataset_5M['open'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_5M['volume'] = dataset_5M['volume'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_5M['OHLC/4'] = dataset_5M['OHLC/4'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_5M['HLCC/4'] = dataset_5M['HLCC/4'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_5M['HLC/3'] = dataset_5M['HLC/3'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_5M['HL/2'] = dataset_5M['HL/2'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_5M['time'] = dataset_5M['time'][cut_first:int(loc_end_5M)].reset_index(drop=True)
+		dataset_pr_5M = dataset_5M.truncate(before=cut_first, after=int(loc_end_5M-1), axis=None, copy=True).reset_index(drop=True)
 
 		location_1H = -1
 		list_time = np.where(
-							(dataset_1H['time'].dt.year.to_numpy() == dataset_5M['time'][int(loc_end_5M)].year) &
-							(dataset_1H['time'].dt.month.to_numpy() == dataset_5M['time'][int(loc_end_5M)].month) &
-							(dataset_1H['time'].dt.day.to_numpy() == dataset_5M['time'][int(loc_end_5M)].day) &
-							(dataset_1H['time'].dt.hour.to_numpy() == dataset_5M['time'][int(loc_end_5M)].hour)
+							(dataset_1H['time'].dt.year.to_numpy() == dataset_5M['time'][int(loc_end_5M-1)].year) &
+							(dataset_1H['time'].dt.month.to_numpy() == dataset_5M['time'][int(loc_end_5M-1)].month) &
+							(dataset_1H['time'].dt.day.to_numpy() == dataset_5M['time'][int(loc_end_5M-1)].day) &
+							(dataset_1H['time'].dt.hour.to_numpy() == dataset_5M['time'][int(loc_end_5M-1)].hour)
 							)[0]
 		try:
 			location_1H = list_time[0] + 1
@@ -102,15 +55,6 @@ class DataChanger:
 		if location_1H >= length_1H:
 			cut_first_1H = location_1H - length_1H
 
-		dataset_pr_1H['low'] = dataset_1H['low'][cut_first_1H:location_1H].reset_index(drop=True)
-		dataset_pr_1H['high'] = dataset_1H['high'][cut_first_1H:location_1H].reset_index(drop=True)
-		dataset_pr_1H['close'] = dataset_1H['close'][cut_first_1H:location_1H].reset_index(drop=True)
-		dataset_pr_1H['open'] = dataset_1H['open'][cut_first_1H:location_1H].reset_index(drop=True)
-		dataset_pr_1H['volume'] = dataset_1H['volume'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_1H['OHLC/4'] = dataset_1H['OHLC/4'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_1H['HLCC/4'] = dataset_1H['HLCC/4'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_1H['HLC/3'] = dataset_1H['HLC/3'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_1H['HL/2'] = dataset_1H['HL/2'][cut_first:int(loc_end_5M)].reset_index(drop=True)
-		dataset_pr_1H['time'] = dataset_1H['time'][cut_first:int(loc_end_5M)].reset_index(drop=True)
+		dataset_pr_1H = dataset_1H.truncate(before=cut_first_1H, after=int(location_1H-1), axis=None, copy=True).reset_index(drop=True)
 
 		return dataset_pr_5M, dataset_pr_1H

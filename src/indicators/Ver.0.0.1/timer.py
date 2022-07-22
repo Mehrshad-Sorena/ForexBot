d332a3687bb18fd13113c9e30204030277353158
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-22 20:45:44 +0430

    Create timer.py

---
+++ src/indicators/Ver.0.0.1/timer.py
@@ -0,0 +1,17 @@
+import time
+
+
+def stTime(func):
+    """
+        st decorator to calculate the total time of a func
+    """
+
+    def stFunc(*args, **keyArgs):
+        t1 = time.time()
+        r = func(*args, **keyArgs)
+        t2 = time.time()
+        print("Function=%s, Time=%s" % (func.__name__, t2 - t1))
+        print()
+        return r
+
+    return stFunc

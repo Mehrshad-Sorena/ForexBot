61e735581265643b037c970707dd4f57557fe156
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-22 20:44:40 +0430

    Create Find_extreme_points.py

---
+++ src/indicators/Ver.0.0.1/Find_extreme_points.py
@@ -0,0 +1,28 @@
+import numpy as np
+from scipy.signal import argrelextrema
+import pandas as pd
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+rs = np.random.randn(200)
+xs = [0]
+for r in rs:
+   xs.append(xs[-1] * 0.9 + r)
+
+df = pd.DataFrame(xs, columns=['data'])
+print(df)
+n = 5  # number of points to be checked before and after
+
+# Find local peaks
+
+df['min'] = df.iloc[argrelextrema(df.data.values, np.less_equal,
+                    order=n)[0]]['data']
+df['max'] = df.iloc[argrelextrema(df.data.values, np.greater_equal,
+                    order=n)[0]]['data']
+
+# Plot results
+
+plt.scatter(df.index, df['min'], c='r')
+plt.scatter(df.index, df['max'], c='g')
+plt.plot(df.index, df['data'])
+plt.show()

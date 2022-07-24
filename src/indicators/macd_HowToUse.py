a271387fad8f7b6c9b87101e9ed5195bc66418fc
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-24 19:12:53 +0430

    Create macd_HowToUse.py

---
+++ src/indicators/macd_HowToUse.py
@@ -0,0 +1,26 @@
+from Mt5_LoginGetData import LoginGetData as getdata
+from macd_Divergence import Divergence
+from macd_Tester import Tester
+from macd_Parameters import Parameters
+from macd_Config import Config
+import pandas as pd
+
+loging = getdata()
+
+
+parameters = Parameters()
+config = Config()
+
+parameters.elements['dataset_5M'], parameters.elements['dataset_1H'] = loging.readall(symbol = 'XAUUSD_i', number_5M = 99800, number_1H = 8323)
+parameters.elements['Divergence_symbol'] = 'XAUUSD_i'
+parameters.elements['Divergence_apply_to'] = 'close'
+
+macd = Divergence(parameters = parameters, config = config)
+signal, signaltype = macd.divergence(sigtype = 'buy', sigpriority = 'primary')
+
+macd_tester = Tester(parameters = parameters, config = config)
+
+signal_out = macd_tester.ProfitFlagFinder(signal = signal, sigtype = signaltype, flaglearn = True, flagtest = True)
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+	print(signal_out)

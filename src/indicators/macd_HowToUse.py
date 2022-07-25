5bc3d5df1194620f42e8a5a0c58b902e1eea7d9c
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-25 15:51:45 +0430

    Update macd_HowToUse.py

--- src/indicators/macd_HowToUse.py
+++ src/indicators/macd_HowToUse.py
@@ -1,9 +1,12 @@
 from Mt5_LoginGetData import LoginGetData as getdata
-from macd_Divergence import Divergence
-from macd_Tester import Tester
+from indicator_Divergence import Divergence
+from indicator_Tester import Tester
 from macd_Parameters import Parameters
 from macd_Config import Config
+from macd_MACD import MACD
 import pandas as pd
+from indicator_Parameters import Parameters as indicator_parameters
+from indicator_Config import Config as indicator_config
 
 loging = getdata()
 
@@ -11,12 +14,29 @@ loging = getdata()
 parameters = Parameters()
 config = Config()
 
-parameters.elements['dataset_5M'], parameters.elements['dataset_1H'] = loging.readall(symbol = 'XAUUSD_i', number_5M = 99800, number_1H = 8323)
-parameters.elements['Divergence_symbol'] = 'XAUUSD_i'
-parameters.elements['Divergence_apply_to'] = 'close'
+ind_params = indicator_parameters()
+ind_config = indicator_config()
 
-macd = Divergence(parameters = parameters, config = config)
-signal, signaltype = macd.divergence(sigtype = 'buy', sigpriority = 'primary')
+
+
+parameters.elements['dataset_5M'], parameters.elements['dataset_1H'] = loging.readall(symbol = 'XAUUSD_i', number_5M = 4000, number_1H = 8323)
+parameters.elements['MACD_symbol'] = 'XAUUSD_i'
+parameters.elements['MACD_apply_to'] = 'close'
+
+macd = MACD(parameters = parameters, config = config)
+macd_calc = macd.calculator_macd()
+
+macd = Divergence(parameters = ind_params, config = ind_config)
+signal, signaltype = macd.divergence(
+									sigtype = 'buy',
+									sigpriority = 'primary',
+									indicator = macd_calc,
+									column_div = 'macds',
+									ind_name = 'macd',
+									dataset_5M = parameters.elements['dataset_5M'],
+									dataset_1H = parameters.elements['dataset_1H'],
+									symbol = 'XAUUSD_i',
+									)
 
 macd_tester = Tester(parameters = parameters, config = config)
 

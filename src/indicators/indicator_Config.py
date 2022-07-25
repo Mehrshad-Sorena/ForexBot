940805c54b15a428378ec1a082b7684c6ff1a715
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-25 15:51:31 +0430

    Create indicator_Config.py

---
+++ src/indicators/indicator_Config.py
@@ -0,0 +1,19 @@
+
+
+class Config:
+
+	def __init__(cls):
+		
+		cls.cfg = dict({
+
+						#************** Divergence:
+
+
+						'Divergence' + '_buy_doing': True,
+						'Divergence' + '_sell_doing': True,
+						'Divergence' + '_primary_doing': True,
+						'Divergence' + '_secondry_doing': True,
+
+						#/////////////////////////////
+
+						})

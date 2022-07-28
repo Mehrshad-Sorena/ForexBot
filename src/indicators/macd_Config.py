465f2f217c099e1e56f2b07c6db20dd4631fe0a8
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-28 16:57:17 +0430

    Update macd_Config.py

--- src/indicators/macd_Config.py
+++ src/indicators/macd_Config.py
@@ -8,16 +8,9 @@ class Config:
 
 						#************** Divergence:
 
-						'Divergence' + '_mode': 'online',
-						'Divergence' + '_plot': False,
-						'Divergence' + '_buy_doing': True,
-						'Divergence' + '_sell_doing': True,
-						'Divergence' + '_primary_doing': True,
-						'Divergence' + '_secondry_doing': True,
-						'Divergence' + '_name_stp_pr': True,
-						'Divergence' + '_real_test': False,
-						'Divergence' + '_flag_learning': False,
-						'Divergence' + '_pic_save': False,
+						'path_society': 'GL/society/',
+						'path_graveyard': 'GL/graveyard/',
+						'path_superhuman': 'GL/superhuman/',
 
 						#/////////////////////////////
 

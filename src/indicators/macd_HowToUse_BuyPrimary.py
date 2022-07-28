71aef9ac55021a74a65e5648b7f7d6ae55066c2f
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-07-28 16:57:21 +0430

    Create macd_HowToUse_BuyPrimary.py

---
+++ src/indicators/macd_HowToUse_BuyPrimary.py
@@ -0,0 +1,32 @@
+from Mt5_LoginGetData import LoginGetData as getdata
+from macd_Parameters import Parameters
+from macd_Config import Config
+from macd_MACD import MACD
+import pandas as pd
+
+
+
+loging = getdata()
+
+
+parameters = Parameters()
+config = Config()
+
+# ind_params = indicator_parameters()
+# ind_config = indicator_config()
+
+
+
+parameters.elements['dataset_5M'], parameters.elements['dataset_1H'] = loging.readall(symbol = 'XAUUSD_i', number_5M = 99800, number_1H = 8323)
+parameters.elements['MACD_symbol'] = 'XAUUSD_i'
+parameters.elements['MACD_apply_to'] = 'close'
+
+macd = MACD(parameters = parameters, config = config)
+macd_calc = macd.Genetic(
+						dataset_5M = parameters.elements['dataset_5M'], 
+						dataset_1H = parameters.elements['dataset_1H'], 
+						symbol = 'XAUUSD_i', 
+						signaltype = 'buy', 
+						signalpriority = 'primary', 
+						num_turn = 2000
+						)

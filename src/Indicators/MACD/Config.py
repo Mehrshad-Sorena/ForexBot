036093f3d7d9e61471a7b8f16a1a222cbdd882d1
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 12:43:41 +0430

    Create Config.py

---
+++ src/Indicators/MACD/Config.py
@@ -0,0 +1,18 @@
+
+
+class Config:
+
+	def __init__(cls):
+		
+		cls.cfg = dict({
+
+						#************** Divergence:
+
+						'path_society': 'GeneticLearning_DB/society/',
+						'path_graveyard': 'GeneticLearning_DB/graveyard/',
+						'path_superhuman': 'GeneticLearning_DB/superhuman/',
+						'path_elites': 'GeneticLearning_DB/elites/',
+
+						#/////////////////////////////
+
+						})

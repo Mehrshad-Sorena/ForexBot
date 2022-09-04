0e15c038c72e3ea917d58a1908c58781ee133dbd
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 12:07:24 +0430

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,23 @@
-from src.forex.forex import ForexRobot
+from src.Indicators.MACD import (
+								MACD,
+								Parameters,
+								Config,
+								Chromosome,
+								ParameterLimits,
+								)
+
+from src.Indicators.StochAstic import (
+										StochAstic,
+										Parameters,
+										Config,
+										Chromosome,
+										ParameterLimits,
+										)
+
+from src.Indicators.RSI import (
+								RSI,
+								Parameters,
+								Config,
+								Chromosome,
+								ParameterLimits,
+								)

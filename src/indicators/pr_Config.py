6e5b3610ebc1aaf15ac2b857103efa1b6a040317
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 18:34:24 +0430

    Create pr_Config.py

---
+++ src/indicators/pr_Config.py
@@ -0,0 +1,57 @@
+class Config:
+
+	def __init__(cls):
+		
+		cls.cfg = dict(
+						{
+						#Config For ExtremePoints:
+						'ExtremePoints_status': True,
+						'ExtremePoints_T_5M': True,
+						'ExtremePoints_T_1H': True,
+						#///////////////////////////
+
+
+						#Config For TrendLines:
+						'TrendLines_status': True,
+
+						'TrendLines_T_5M': True,
+
+						'TrendLines_long_T_5M': True,
+						'TrendLines_mid_T_5M': True,
+						'TrendLines_short1_T_5M': True,
+						'TrendLines_short2_T_5M': True,
+
+						'TrendLines_T_1H': True,
+
+						'TrendLines_long_T_1H': True,
+						'TrendLines_mid_T_1H': True,
+						'TrendLines_short1_T_1H': True,
+						'TrendLines_short2_T_1H': True,
+
+						'TrendLines_plot': False,
+						#///////////////////////////
+
+						#Config For IchimokouFlatLines:
+
+						'IchimokouFlatLines' + '_T_5M': True,
+						'IchimokouFlatLines' + '_T_1H': True,
+						'IchimokouFlatLines' + '_status': True,
+
+						'IchimokouFlatLines_plot': True,
+
+						#//////////////////////////
+
+
+						#Config For Test:
+
+						'Tester_flag_realtest': False,
+
+						'Tester_plot_save': True,
+
+						#/////////////////////////
+
+
+						#Config For
+						'plot': True,
+						}
+						)

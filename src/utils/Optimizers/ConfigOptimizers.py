580d577387c6c3d58324862ba38642c443851261
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 12:55:40 +0430

    Create ConfigOptimizers.py

---
+++ src/utils/Optimizers/ConfigOptimizers.py
@@ -0,0 +1,13 @@
+class Config:
+
+	def __init__(cls):
+		
+		cls.cfg = dict(
+						{
+						#Config For ExtremePoints:
+						'path_macd': 'optimize/macd',
+						'path_stochastic': 'optimize/stochastic',
+						'path_rsi': 'optimize/rsi',
+						#///////////////////////////
+						}
+						)

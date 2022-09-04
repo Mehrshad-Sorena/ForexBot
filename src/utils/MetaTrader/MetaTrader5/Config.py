0da730de02fee4035e74a410968e3d7f6902b52d
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 13:36:59 +0430

    Revert "Delete Config.py"
    This reverts commit 2426a7e064e3ce874fc31e08c13f86195b618c3b.

---
+++ src/utils/MetaTrader/MetaTrader5/Config.py
@@ -0,0 +1,70 @@
+from configparser import RawConfigParser
+import sys
+
+def globalConfig(filename='config.ini', section='global'):
+    parser = RawConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(
+            section, filename))
+
+    return db
+
+
+def accountConfig(filename='/config.ini', section='accounts'):
+    parser = RawConfigParser()
+    parser.read(filename)
+    print(parser.items())
+
+    accounts = dict()
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(
+            section, filename))
+
+    name = db.get('name').split(',')
+    users = db.get('user').split(',')
+    password = db.get('pass').split(',')
+    types = db.get('type').split(',')
+    for n, u, p, t in zip(name, users, password, types):
+        accounts.update(
+                {
+                    n: {
+                        'username': u,
+                        'password': p,
+                        'type': t
+                    }
+                }
+        )
+    return accounts
+
+
+def mongoConfig(filename='config.ini', section='mongodb'):
+    parser = RawConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(
+            section, filename))
+
+    return db
+
+print('account = ',accountConfig())

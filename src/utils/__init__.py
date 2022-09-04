089cbae6cde8efe65c57dce3d733d40594cf4a1f
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 12:54:41 +0430

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,7 @@
-from .logger import logs
-from .resist_protect import (
-        extreme_points,
-        extreme_points_ichimoko,
-        extreme_points_ramp_lines,
-        best_extreme_finder,
-        protect_resist,
-)
+from Divergence import (
+								BestFinder,
+								Config,
+								Divergence,
+								Parameters,
+								Tester,
+							)

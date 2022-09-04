6a685cfb07f1447cc5a7b2487d11598f4b5c21fa
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 18:33:22 +0430

    Create macd_GetPermit_HowToUse.py

---
+++ src/indicators/macd_GetPermit_HowToUse.py
@@ -0,0 +1,38 @@
+from Mt5_LoginGetData import LoginGetData as getdata
+from macd_Parameters import Parameters
+from macd_Config import Config
+from macd_MACD import MACD
+import pandas as pd
+
+
+
+loging = getdata()
+# loging.account_name = 'mehrshadpc'
+# loging.initilizer()
+# loging.login()
+
+
+parameters = Parameters()
+config = Config()
+
+# ind_params = indicator_parameters()
+# ind_config = indicator_config()
+
+
+
+parameters.elements['dataset_5M'], parameters.elements['dataset_1H'] = loging.readall(symbol = 'ETHUSD_i', number_5M = 'all', number_1H = 'all')
+
+parameters.elements['symbol'] = 'ETHUSD_i'
+parameters.elements['MACD_apply_to'] = 'close'
+
+#print(parameters.elements['dataset_1H']['ETHUSD_i'])
+
+macd = MACD(parameters = parameters, config = config)
+macd_calc = macd.GetPermit(
+						dataset_5M = parameters.elements['dataset_5M'],
+						dataset_1H = parameters.elements['dataset_1H'], 
+						symbol = 'ETHUSD_i',
+						signaltype = 'buy',
+						signalpriority = 'primary',
+						flag_savepic = False
+						)

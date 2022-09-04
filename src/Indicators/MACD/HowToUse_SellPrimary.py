40ff88264ba6d8b69dcc8282e5bd770efc2db9cd
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 12:45:12 +0430

    Create HowToUse_SellPrimary.py

---
+++ src/Indicators/MACD/HowToUse_SellPrimary.py
@@ -0,0 +1,41 @@
+from Mt5_LoginGetData import LoginGetData as getdata
+from .Parameters import Parameters
+from .Config import Config
+from .MACD import MACD
+import pandas as pd
+
+
+
+loging = getdata()
+
+
+parameters = Parameters()
+config = Config()
+
+# ind_params = indicator_parameters()
+# ind_config = indicator_config()
+
+
+
+parameters.elements['dataset_5M'], parameters.elements['dataset_1H'] = loging.readall(symbol = 'XAUUSD_i', number_5M = 'all', number_1H = 'all')
+parameters.elements['symbol'] = 'XAUUSD_i'
+parameters.elements['MACD_apply_to'] = 'close'
+
+macd = MACD(parameters = parameters, config = config)
+macd_calc = macd.Genetic(
+						dataset_5M = parameters.elements['dataset_5M'], 
+						dataset_1H = parameters.elements['dataset_1H'], 
+						symbol = 'XAUUSD_i', 
+						signaltype = 'sell', 
+						signalpriority = 'primary', 
+						num_turn = 40
+						)
+
+macd_calc = macd.GetPermit(
+						dataset_5M = parameters.elements['dataset_5M'],
+						dataset_1H = parameters.elements['dataset_1H'], 
+						symbol = 'XAUUSD_i',
+						signaltype = 'sell',
+						signalpriority = 'primary',
+						flag_savepic = False
+						)
